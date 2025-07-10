e2ef03e9e59b44cc3beb1a6edc85af5dcdd7ccad
Ghstslk <Boroha97@gmail.com>
2025-07-10 22:34:32 +0300

    SecurityConfig

---
+++ src/main/java/my/spring/crud/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package my.spring.crud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
---
+++ src/main/java/my/spring/crud/models/Role.java
@@ -0,0 +1,49 @@
+package my.spring.crud.models;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
